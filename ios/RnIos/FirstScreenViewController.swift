42a18f04d29034c4b8fb8c5ef2d0563b7ea6871b
Marcio Bueno <marcio_bueno@apple.com>
2021-12-20 23:27:55 -0300

    renamed ios to iosOriginal and moving the projet to integrate to ios folder

---
+++ ios/RnIos/FirstScreenViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  RnIos
+//
+//  Created by Marcio Bueno on 19/12/21.
+//
+
+import UIKit
+
+class FirstScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
---
+++ iosOriginal/Pods/Target Support Files/Pods-rnIntegration-rnIntegrationTests/Pods-rnIntegration-rnIntegrationTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rnIntegration_rnIntegrationTests : NSObject
+@end
+@implementation PodsDummy_Pods_rnIntegration_rnIntegrationTests
+@end
